9d5d40983ab9a7a1fe2ccbf3e30ead26bd90a2bf
John Francis Ballarda <jballarda@gbox.adnu.edu.ph>
2026-07-21 16:50:28 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.56"],
+  allowedDevOrigins: ["192.168.1.56", "192.168.1.50", "192.168.1.53"],
   turbopack: {
     root: __dirname,
   },
